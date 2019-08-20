69fa059635e3465d8acdf683e33b1a80e6cda1cd
Armando Roman <armandoroman1016@gmail.com>
2019-08-20 12:51:32 -0700

    redux store setup

--- friends/src/index.js
+++ friends/src/index.js
@@ -2,11 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, applyMiddleWare, compose} from 'redux'
+import logger from 'logger'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+import { red } from 'ansi-colors';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSIONS_CHROME__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleWare(thunk, logger)))
+
+
+ReactDOM.render(
+    <Provider store = {store}>
+        <Router>
+            <App />
+        </Router>
+    </Provider>, 
+    document.getElementById('root'));
+
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,12 @@
+const initialState = {
+    friendsList : [],
+    isLoading: [],
+    error:'',
+    fireRedirect: false,
+}
+export const reducer = (state = initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
